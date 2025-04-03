995fa53269fddb8396964e26d09aa09c9c96697e
NamjaJr <hanruslan144@gmail.com>
2025-04-03 16:35:52 +0600

    new commit

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,30 @@
-// MOVE BLOCK HW 1.2
+const gmailInput = document.querySelector('#gmail_input')
+const gmailButton = document.querySelector('#gmail_button')
+const gmailResult = document.querySelector('#gmail_result')
 
-const parentBlock = document.querySelector('.parent_block');
-const childBlock = document.querySelector('.child_block');
+const regExp = /^[a-zA_Z0-9.+%-]+@gmail\.com$/
 
-let positionX = 0
-let positionY = 0
-
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
-
-const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
+gmailButton.onclick = () => {
+    if (regExp.test(gmailInput.value)){
+        gmailResult.innerHTML = 'OK'
+        gmailResult.style.color = 'green'
+    }else {
+        gmailResult.innerHTML = 'ERROR'
+        gmailResult.style.color = 'red'
+    }
 }
 
-moveBlock();
+
+
+const parentBlock = document.querySelector('.parent_block')
+const childBlock = document.querySelector('.child_block')
+let blockPosition = 0
+
+const moveBlock = () => {
+    blockPosition += 2
+    childBlock.style.left =` ${blockPosition}px`
+    if (blockPosition < parentBlock.clientWidth - childBlock.offsetWidth){
+        requestAnimationFrame(moveBlock)
+    }
+}
+moveBlock()
