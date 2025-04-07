fadc878068ee9447e19bdafd3f711560d65bcc05
NamjaJr <hanruslan144@gmail.com>
2025-04-07 16:28:10 +0600

    dz2

--- js/home_works.js
+++ js/home_works.js
@@ -18,13 +18,71 @@ gmailButton.onclick = () => {
 
 const parentBlock = document.querySelector('.parent_block')
 const childBlock = document.querySelector('.child_block')
-let blockPosition = 0
+let positionX = 0,positionY = 0
+
+const maxWidth =  parentBlock.clientWidth - childBlock.clientWidth
+const maxHeight =  parentBlock.clientHeight - childBlock.clientHeight
+
+let moveRight = maxWidth
+let moveTop = maxHeight
 
 const moveBlock = () => {
-    blockPosition += 2
-    childBlock.style.left =` ${blockPosition}px`
-    if (blockPosition < parentBlock.clientWidth - childBlock.offsetWidth){
+    if (positionX < maxWidth){
+        positionX += 2
+        childBlock.style.left = `${positionX}px`
+        requestAnimationFrame(moveBlock)
+    }else if(positionX >= maxWidth && positionY < maxHeight){
+        positionY += 2
+        childBlock.style.top = `${positionY}px`
+        requestAnimationFrame(moveBlock)
+    }else if(positionY >= maxHeight && moveRight > 0){
+        moveRight -= 2
+        childBlock.style.left = `${moveRight}px`
+        requestAnimationFrame(moveBlock)
+    }else if (moveRight <= 0 && moveTop > 0){
+        moveTop -= 2
+        childBlock.style.top = `${moveTop}px`
+        requestAnimationFrame(moveBlock)
+    }else if(moveTop <= 0){
+        positionY = 0
+        positionX = 0
+        moveRight = maxWidth
+        moveTop = maxHeight
         requestAnimationFrame(moveBlock)
     }
+
+
 }
 moveBlock()
+
+
+
+const seconds = document.querySelector('#seconds')
+
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
+const reset = document.querySelector('#reset')
+
+let timer;
+let count = 0
+
+start.onclick = () => {
+    if (!timer){
+        timer = setInterval(() => {
+            count += 1
+            seconds.innerHTML = count
+        },1000)
+    }
+}
+
+stop.onclick = () => {
+    clearInterval(timer)
+    timer = 0
+}
+
+reset.onclick = () => {
+    clearInterval(timer)
+    timer = null
+    count = 0
+    seconds.innerHTML = count
+};
